ffdd46bc86dd94ef7c7c4d54e199d50c507cbd42
CSL <uceshej@ucl.ac.uk>
2020-12-08 23:10:45 +0000

    2

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-full_name=input('please input your full name')
-print(len(full_name))
+def main(a,b,c):
+	
+	x=float(a)
+	y=float(b)
+	z=float(c)
+	if x+y+z!=180:
+	    typ='error'
+	elif x==y and y==z:
+	    typ='eq'
+	elif x==y or x==z or y==z:
+	    typ='is'
+	else:
+	    typ='sca'
+	return typ
+
+print(main(60,60,60))
+print(main(70,70,40))
+print(main(56,84,40))
+print(main(46,79,76))
+
+if __name__=='__main__':
+
+        main(1,2,3)
